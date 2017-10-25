b4341aa02392c181f17144511b050421cf6a2fbd
Krzysztof Kleina <k.kleina@wp.eu>
2017-10-25 13:50:41 +0200

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+// alert('Hello, ' + name);
+/* 
+nie 
+ma
+to 
+jak 
+zadanie 
+dotyczące 
+komentarzy
+*/
+// xD
 console.log('Hello, ' + name);
