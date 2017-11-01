4fbf9549c6d40538bbc13d95cd190ae5a603a8f4
Krzysztof Kleina <k.kleina@wp.eu>
2017-11-01 14:02:11 +0100

    m9.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
-var triangle1Area = getTriangleArea(10, 15),
-    triangle2Area = getTriangleArea(5, 12),
-    triangle3Area = getTriangleArea(4, 6);
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
+    maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'],
+    allNames = femaleNames.concat(maleNames),
+    newName = 'Marian';
 
-function getTriangleArea(a, h) {
-    if ((a && h) > 0) {
-        return a +'*'+ h +'/2 = '+ a*h/2;
-    } else {
-        return 'Nieprawidłowe dane!';
-    }
+if (allNames.indexOf(newName) === -1) {
+    allNames.push(newName);
+} else {
+    console.log(newName + ' już jest w tablicy.');
 }
 
-console.log(getTriangleArea(10, 6));
-console.log(triangle1Area +'\n'+ triangle2Area +'\n'+ triangle3Area);
-
-
+console.log(allNames);
