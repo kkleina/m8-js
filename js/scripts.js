c3f57277dad844dba7c2912f8e6a530761e1f7e6
Krzysztof Kleina <k.kleina@wp.eu>
2017-11-02 13:19:36 +0100

    m9.4 Tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.',
-    dinosaur = 'triceratops',
-    dinosaur = dinosaur.toUpperCase(),
-    textReplace = text.replace('Velociraptor', dinosaur);
+function drawTree(heightTree) {
+    for (var i = 0 ; i < heightTree ; i++) {
+        var star = '*';
+        var space = '';
+        for (var k = heightTree ; k > i ; k--) {
+            space += ' ';
+        }
+        for (var j = 0 ; j < i ; j++) {
+            star += '**';
+        }
+        console.log(space + star);
+    }
+}
 
-console.log(textReplace.substr(0, textReplace.length/2));
+drawTree(7);
