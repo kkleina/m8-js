b369304d1a5cf7a7acb6959a0cf597669929cc66
Krzysztof Kleina <k.kleina@wp.eu>
2017-11-02 14:12:54 +0100

    m9.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,5 @@
-function drawTree(heightTree) {
-    for (var i = 0 ; i < heightTree ; i++) {
-        var star = '*';
-        var space = '';
-        for (var k = heightTree ; k > i ; k--) {
-            space += ' ';
-        }
-        for (var j = 0 ; j < i ; j++) {
-            star += '**';
-        }
-        console.log(space + star);
-    }
-}
+var buttons = document.getElementsByClassName('button');
 
-drawTree(7);
+for (i=0 ; i < buttons.length ; i++) {
+    alert(buttons[i].innerText);
+}
